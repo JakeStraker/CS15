a434dac763b237408e5aec2a2dd658764e841e16
JakeStraker <jake.straker@live.co.uk>
2015-03-31 21:04:04 +0100

    Adding all Files

---
+++ Group Stegonography Project/frmMain.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Group_Stegonography_Project
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pBoxMain = new System.Windows.Forms.PictureBox();
+            this.stripFile = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.embedHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extractionHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtSecretText = new System.Windows.Forms.TextBox();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.btnEmbed = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblImgSize = new System.Windows.Forms.Label();
+            this.lblFileMax = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxMain)).BeginInit();
+            this.stripFile.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pBoxMain
+            // 
+            this.pBoxMain.Location = new System.Drawing.Point(12, 70);
+            this.pBoxMain.Name = "pBoxMain";
+            this.pBoxMain.Size = new System.Drawing.Size(350, 250);
+            this.pBoxMain.TabIndex = 0;
+            this.pBoxMain.TabStop = false;
+            // 
+            // stripFile
+            // 
+            this.stripFile.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.stripFile.Location = new System.Drawing.Point(0, 0);
+            this.stripFile.Name = "stripFile";
+            this.stripFile.Size = new System.Drawing.Size(373, 24);
+            this.stripFile.TabIndex = 1;
+            this.stripFile.Text = "menuStrip1";
+            this.stripFile.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.stripFile_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearAllToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // clearAllToolStripMenuItem
+            // 
+            this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.clearAllToolStripMenuItem.Text = "Clear All";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.embedHelpToolStripMenuItem,
+            this.extractionHelpToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // embedHelpToolStripMenuItem
+            // 
+            this.embedHelpToolStripMenuItem.Name = "embedHelpToolStripMenuItem";
+            this.embedHelpToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.embedHelpToolStripMenuItem.Text = "Embedding Help";
+            // 
+            // extractionHelpToolStripMenuItem
+            // 
+            this.extractionHelpToolStripMenuItem.Name = "extractionHelpToolStripMenuItem";
+            this.extractionHelpToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.extractionHelpToolStripMenuItem.Text = "Extraction Help";
+            // 
+            // txtSecretText
+            // 
+            this.txtSecretText.Location = new System.Drawing.Point(12, 392);
+            this.txtSecretText.Multiline = true;
+            this.txtSecretText.Name = "txtSecretText";
+            this.txtSecretText.Size = new System.Drawing.Size(350, 100);
+            this.txtSecretText.TabIndex = 2;
+            this.txtSecretText.Text = "Text to be embedded in the image";
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Image = ((System.Drawing.Image)(resources.GetObject("btnExtract.Image")));
+            this.btnExtract.Location = new System.Drawing.Point(12, 326);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(154, 36);
+            this.btnExtract.TabIndex = 3;
+            this.btnExtract.Text = "Extract Data";
+            this.btnExtract.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // btnEmbed
+            // 
+            this.btnEmbed.Image = ((System.Drawing.Image)(resources.GetObject("btnEmbed.Image")));
+            this.btnEmbed.Location = new System.Drawing.Point(208, 326);
+            this.btnEmbed.Name = "btnEmbed";
+            this.btnEmbed.Size = new System.Drawing.Size(154, 36);
+            this.btnEmbed.TabIndex = 4;
+            this.btnEmbed.Text = "Embed Data";
+            this.btnEmbed.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnEmbed.UseVisualStyleBackColor = true;
+            this.btnEmbed.Click += new System.EventHandler(this.btnEmbed_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(12, 27);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(350, 37);
+            this.btnSelect.TabIndex = 5;
+            this.btnSelect.Text = "Select an Image";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(111, 498);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(157, 30);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblImgSize
+            // 
+            this.lblImgSize.AutoSize = true;
+            this.lblImgSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImgSize.Location = new System.Drawing.Point(12, 365);
+            this.lblImgSize.Name = "lblImgSize";
+            this.lblImgSize.Size = new System.Drawing.Size(102, 20);
+            this.lblImgSize.TabIndex = 7;
+            this.lblImgSize.Text = "Size = 00000";
+            // 
+            // lblFileMax
+            // 
+            this.lblFileMax.AutoSize = true;
+            this.lblFileMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFileMax.Location = new System.Drawing.Point(177, 365);
+            this.lblFileMax.Name = "lblFileMax";
+            this.lblFileMax.Size = new System.Drawing.Size(149, 20);
+            this.lblFileMax.TabIndex = 8;
+            this.lblFileMax.Text = "Max Data = 0 Chars";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(373, 576);
+            this.Controls.Add(this.lblFileMax);
+            this.Controls.Add(this.lblImgSize);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnEmbed);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.txtSecretText);
+            this.Controls.Add(this.pBoxMain);
+            this.Controls.Add(this.stripFile);
+            this.MainMenuStrip = this.stripFile;
+            this.Name = "frmMain";
+            this.Text = "CS15 Steganography Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxMain)).EndInit();
+            this.stripFile.ResumeLayout(false);
+            this.stripFile.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBoxMain;
+        private System.Windows.Forms.MenuStrip stripFile;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem embedHelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extractionHelpToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtSecretText;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.Button btnEmbed;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblImgSize;
+        private System.Windows.Forms.Label lblFileMax;
+    }
+}
+
---
+++ Group Stegonography Project/frmMain.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Group_Stegonography_Project
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "Bitmap Pictures (.bmp)|*.bmp";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.ShowDialog();
+            Globals.filepath = openFileDialog1.FileName;
+            FileStream fs = new FileStream(Globals.filepath, FileMode.Open);
+            Globals.currentImage = (Bitmap)Image.FromStream(fs);
+            fs.Close();
+            pBoxMain.SizeMode = PictureBoxSizeMode.Zoom;
+            pBoxMain.Image = Globals.currentImage;
+            pBoxMain.Refresh();
+            Globals.selectedImage = true;
+            Globals.imgHeight = Globals.currentImage.Height;
+            Globals.imgWidth = Globals.currentImage.Width;
+            lblImgSize.Text = "Size = (" + Globals.imgWidth + " x " + Globals.imgHeight + ")";
+            Globals.maxChar = (int)(Math.Truncate((double)(((Globals.imgHeight * Globals.imgWidth) * 3) / 8)));
+            lblFileMax.Text = "Max Chars = " + Globals.maxChar;
+
+        }
+
+        private void btnEmbed_Click(object sender, EventArgs e)
+        {
+            if (Globals.selectedImage == true && txtSecretText.Text.Length < Globals.maxChar)
+            {
+                Globals.currentImage = functions.embedMessage(txtSecretText.Text, Globals.currentImage);
+                pBoxMain.Image = Globals.currentImage;
+                pBoxMain.Refresh();
+                System.IO.File.Delete(Globals.filepath);
+                Globals.currentImage.Save(Globals.filepath,System.Drawing.Imaging.ImageFormat.Bmp);
+            }
+            else
+            {
+                if (Globals.selectedImage == true)
+                {
+                    MessageBox.Show("You are trying to embed more than the max characters");
+                }
+                else
+                {
+                    MessageBox.Show("You have not selected an Image");
+                }
+            }
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(functions.extractMessage(Globals.currentImage));
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtSecretText.Clear();
+        }
+
+        private void stripFile_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+    public static class Globals
+    {
+        public static bool selectedImage = false;
+        public static Bitmap currentImage;
+        public static string filepath = null;
+        public static int imgHeight = 0;
+        public static int imgWidth = 0;
+        public static int maxChar = 0;
+    }
+
+    public static class functions
+    {
+         public enum Embedding //we will only have two states for the embedding function, Adding real data (text) and data to signify the end (footer)
+        {
+            Text,
+            Footer
+        };
+        public static Bitmap embedMessage(string text, Bitmap image)
+        {
+            Embedding doingNow = Embedding.Text;
+            int charCounter = 0, asciiVal = 0, zeros = 0, R = 0, G = 0, B = 0, bitcounter = 0;
+            for (int y = 0; y < Globals.imgHeight; y++)
+            {
+                // pass through each column
+                for (int x = 0; x < Globals.imgWidth; x++)
+                {   // Each pixel will contain multiple colours, so we need an object (a colour in this case) to manipulate
+                    //MixedColours will hold the values of pixel currently being used
+                    Color pixel = image.GetPixel(x, y);
+
+                    //we need to pre-emptitively clear the values of the Least Significant Bit of each colour
+                    //this function takes a number and subtracts its least significant bit from itself, resulting in the least significant bit being zero
+                    functions.removeLSB(pixel.R, pixel.G, pixel.B, out R, out G, out B);
+                    for (int n = 0; n < 3; n++) //do 3 times per bit, using 3 colours
+                    {
+                        if (newChar(bitcounter) == true) // check if a character(made of 8 bits) has finished being added (or this is the first instance)
+                        {
+                            // check if the whole process has finished
+                            // The function is finished when the footer has finished being added (8 zero's)
+                            if (doingNow == Embedding.Footer && zeros == 8)
+                            {
+                                //if a pixel is only partially covered by a zeros (e.g the last 2 bits of a byte but the third bit (blue) isnt used)
+                                //this will make sure that pixel is saved.
+                                if ((bitcounter - 1) % 3 < 2)
+                                {
+                                    image.SetPixel(x, y, Color.FromArgb(R,G,B));
+                                }
+                                // return the bitmap with the text hidden in
+                                return image;
+                            }
+                            if (charCounter >= text.Length)
+                            {
+                                doingNow = Embedding.Footer; // start adding the footer padding to the image (8 zeros)
+                            }
+                            if (charCounter < text.Length)
+                            {
+                                asciiVal = text[charCounter++];
+                            }
+
+                        }
+                        switch (bitcounter % 3)
+                        {
+                            case 0:
+                                {
+                                    if (doingNow == Embedding.Text)
+                                    {
+                                        embedBit(ref asciiVal, ref R);
+                                    }
+                                    
+                                }break;
+                            case 1:
+                                {
+                                    if (doingNow == Embedding.Text)
+                                    {
+                                        embedBit(ref asciiVal, ref G);
+                                    }
+                                    
+                                }break;
+                            case 2:
+                                {
+                                    if (doingNow == Embedding.Text)
+                                    {
+                                        embedBit(ref asciiVal, ref B);
+                                    }
+                                    image.SetPixel(x, y, Color.FromArgb(R, G, B)); //after the last colour value (blue) has been changed, we can store the new pixel in the image
+                                    
+                                }break;
+                        }
+                        bitcounter++;
+                        if (doingNow == Embedding.Footer)
+                        {
+                            zeros++; //keep incrementing zeros until the if (doingNow == Embedding.Footer && zeros == 8) statement is triggered to return the image
+                        }
+                    }
+
+                }
+            }
+            return image;
+        }
+        public static string extractMessage(Bitmap image)
+        {
+            int bitCounter = 0, asciiVal = 0;
+            StringBuilder extractedText = new StringBuilder();
+            // For each row (in the pixel grid) in the image
+            for (int y = 0; y < Globals.imgHeight; y++)
+            {
+                // Go through every column
+                for (int x = 0; x < Globals.imgWidth; x++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    // for each pixel, run this loop 3 times (representing the 3 colours)
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (bitCounter % 3)
+                        {
+                            case 0:
+                                {
+                                    extractBit(ref asciiVal, pixel.R);
+                                } break;
+                            case 1:
+                                {
+                                    extractBit(ref asciiVal, pixel.G);
+                                } break;
+                            case 2:
+                                {
+                                    extractBit(ref asciiVal, pixel.B);
+                                } break;
+                        }
+
+                        bitCounter++;
+
+                        if (newChar(bitCounter) == true)
+                        {
+                            asciiVal = reverseBits(asciiVal);
+                            if (asciiVal == 0)
+                            {
+                                return extractedText.ToString();
+                            }
+                            else
+                            {
+                                char c = (char)asciiVal;
+                                extractedText.Append(c);
+                            }
+                        }
+                    }
+                }
+            }
+            return "Error: something must have went really wrong here \n (This is most likely not embedded text)";
+        }
+        public static int reverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+        public static void removeLSB(int baseR, int baseG, int baseB, out int R, out int G, out int B)
+        {
+            R = baseR - baseR % 2;
+            G = baseG - baseG % 2;
+            B = baseB - baseB % 2;
+        }
+        public static bool newChar(int count)
+        {
+            if (count % 8 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static void embedBit(ref int ascii, ref int colourVal)
+        {
+            //if we are not adding text (ergo are adding the footer padding) then nothing needs to be added (the LSB has already been cleared)
+            colourVal += ascii % 2;//the rightmost bit in any binary number (lets call it x) can be found by carrying out x mod 2.
+            ascii = ascii / 2;//binary division by 2 represents a bit shift, by moving the numbers one to the right, the next bit of the character can be added using the mod 2 function
+            //(01101100/2) = 00110110
+        }
+        public static void extractBit(ref int ascii, int colourVal)
+        {
+            //multiplication by 2 in binary, similarly to division induces a shift, this case in to the left
+            //(00110110*2) = 01101100
+            //the rightmost bit in any binary number (x) can be found by carrying out x mod 2., so by adding it, 
+            //after multiplication by 2 we are making the newly opened up LSB equal to the LSB of the pixels colour value
+            ascii = ascii * 2 + colourVal % 2;
+        }
+    } 
+    }
